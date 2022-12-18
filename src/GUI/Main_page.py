ec1e3dfaccd3313a6ecc9f50958530cad97e4bbf
fahmy <fahmyniaz@gmail.com>
2022-12-18 21:30:05 +0000

    Main Page create

---
+++ src/GUI/Main_page.py
@@ -0,0 +1,100 @@
+import tkinter as tk
+import tkinter.font as tkf
+from tkinter import Label
+
+from PIL import Image,ImageTk
+from tkinter import *
+
+
+from src.GUI.AdminLogin import AdminLogin
+from src.GUI.CoordinatorLogin import CoordinatorLogin
+from src.GUI.ManagerLogin import ManagerLogin
+
+
+class App:
+    def __init__(self, figure):
+        # setting title
+        figure.title("Solent Trip Main Page")
+        figure["bg"] = "#e3e8cd"
+        # setting window size
+        width = 850
+        height = 250
+        screenwidth = figure.winfo_screenwidth()
+        screenheight = figure.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+        figure.geometry(alignstr)
+        figure.resizable(width=False, height=False)
+
+        title = tk.Label(figure)
+        ft = tkf.Font(family='Arial', size=26)
+        title["bg"] = "#e3e8cd"
+        title["font"] = ft
+        title["fg"] = "#800000"
+        title["justify"] = "center"
+        title["text"] = " Solent Trip Management"
+        title.place(x=0, y=0, width=width, height=52)
+
+        btn_admin = tk.Button(figure)
+        btn_admin["activebackground"] = "#57ef57"
+        btn_admin["anchor"] = "center"
+        btn_admin["bg"] = "#90ee90"
+        btn_admin["bd"] = 1
+        ft = tkf.Font(family='Times', size=22)
+        btn_admin["font"] = ft
+        btn_admin["fg"] = "#000000"
+        btn_admin["justify"] = "center"
+        btn_admin["text"] = "Admin"
+        btn_admin.place(x=20, y=100, width=270, height=100)
+        btn_admin["command"] = lambda: self.btnAdminHandler(figure)
+
+        btn_manager = tk.Button(figure)
+        btn_manager["activebackground"] = "#57ef57"
+        btn_manager["anchor"] = "center"
+        btn_manager["bg"] = "#90ee90"
+        btn_manager["bd"] = 1
+        ft = tkf.Font(family='Times', size=22)
+        btn_manager["font"] = ft
+        btn_manager["fg"] = "#000000"
+        btn_manager["justify"] = "center"
+        btn_manager["text"] = "Manager"
+        btn_manager.place(x=300, y=100, width=250, height=100)
+        btn_manager["command"] = lambda: self.btnManagerHandler(figure)
+
+        btn_coordinator = tk.Button(figure)
+        btn_coordinator["activebackground"] = "#57ef57"
+        btn_coordinator["anchor"] = "center"
+        btn_coordinator["bg"] = "#90ee90"
+        btn_coordinator["bd"] = 1
+        ft = tkf.Font(family='Times', size=22)
+        btn_coordinator["font"] = ft
+        btn_coordinator["fg"] = "#000000"
+        btn_coordinator["justify"] = "center"
+        btn_coordinator["text"] = "Coordinator"
+        btn_coordinator.place(x=560, y=100, width=250, height=100)
+        btn_coordinator["command"] = lambda: self.btnCoordinatorHandler(figure)
+
+
+
+    @staticmethod
+    def btnAdminHandler(figure):
+        adminLoginFigure = tk.Toplevel(figure)
+        AdminLogin(adminLoginFigure)
+        adminLoginFigure.mainloop()
+
+    @staticmethod
+    def btnManagerHandler(figure):
+        managerLoginFigure = tk.Toplevel(figure)
+        ManagerLogin(managerLoginFigure)
+        managerLoginFigure.mainloop()
+
+    @staticmethod
+    def btnCoordinatorHandler(figure):
+        coorLoginFigure = tk.Toplevel(figure)
+        CoordinatorLogin(coorLoginFigure)
+        coorLoginFigure.mainloop()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
